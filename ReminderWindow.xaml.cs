f02a42b6c0cf34a25b61848edfa620c68765982e
Assaf Itzikson <sysqo82@gmail.com>
2026-04-19 08:17:39 +0100

    fix: height of reminder and location, app shortcut
    feature: added an application icon for the shortcut

--- ReminderWindow.xaml.cs
+++ ReminderWindow.xaml.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Media;
 using System.Windows;
 using System.Windows.Threading;
@@ -13,10 +14,17 @@ namespace PCLockScreen
     {
         private DispatcherTimer autoDismissTimer;
 
+        // Track all open reminder windows so we can stack them vertically
+        private static readonly List<ReminderWindow> _openWindows = new List<ReminderWindow>();
+        private const double StackGap = 10;
+
         public ReminderWindow(string message, bool persistent = false)
         {
             InitializeComponent();
             ReminderMessage.Text = message ?? "Reminder";
+
+            // Position after layout so ActualWidth/Height are available
+            Loaded += (_, __) => PositionWindow();
             
             // Play notification sound
             try
@@ -51,12 +59,31 @@ namespace PCLockScreen
             Close();
         }
 
+        private void PositionWindow()
+        {
+            var screen = System.Windows.SystemParameters.WorkArea;
+            double centreLeft = screen.Left + (screen.Width - ActualWidth) / 2;
+
+            // Stack below all existing open reminder windows
+            double top = screen.Top + 40;
+            foreach (var w in _openWindows)
+            {
+                double bottom = w.Top + w.ActualHeight + StackGap;
+                if (bottom > top) top = bottom;
+            }
+
+            Left = centreLeft;
+            Top  = top;
+            _openWindows.Add(this);
+        }
+
         protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
         {
             if (autoDismissTimer != null)
             {
                 autoDismissTimer.Stop();
             }
+            _openWindows.Remove(this);
             base.OnClosing(e);
         }
 
